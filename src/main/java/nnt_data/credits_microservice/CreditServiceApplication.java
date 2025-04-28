52c728a42ae707fcc6865fc758650299bc00dce2
Andrea Pilar Llerena Zuñiga <allerenz@emeal.nttdata.com>
2025-04-27 20:31:14 -0500

    feat: properties

--- src/main/java/nnt_data/credits_microservice/CreditServiceApplication.java
+++ src/main/java/nnt_data/credits_microservice/CreditServiceApplication.java
@@ -2,8 +2,10 @@ package nnt_data.credits_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CreditServiceApplication {
 
 	public static void main(String[] args) {
